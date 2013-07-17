3f43802c862c91a5d2c0d729ac34ef2a43155532
Paulson McIntyre <pmcintyre@salesforce.com>
2013-07-17 17:53:36 -0400

    Progress towards adding expect-like capabilities

--- fabric/state.py
+++ fabric/state.py
@@ -318,6 +318,7 @@ env = _AttributeDict({
     'path': '',
     'path_behavior': 'append',
     'port': default_port,
+    'prompt_responses' : {},
     'real_fabfile': None,
     'remote_interrupt': None,
     'roles': [],
@@ -325,7 +326,7 @@ env = _AttributeDict({
     'shell_env': {},
     'skip_bad_hosts': False,
     'ssh_config_path': default_ssh_config_path,
-    'ok_ret_codes': [0],     # a list of return codes that indicate success
+    'ok_ret_codes': [0],  # a list of return codes that indicate success
     # -S so sudo accepts passwd via stdin, -p with our known-value prompt for
     # later detection (thus %s -- gets filled with env.sudo_prompt at runtime)
     'sudo_prefix': "sudo -S -p '%(sudo_prompt)s' ",
