a5128916364211c9fdc059507fddd29b28ebbe1a
Paulson McIntyre <pmcintyre@salesforce.com>
2014-07-21 13:50:30 -0400

    Allow callables for finding a password to try

--- fabric/auth.py
+++ fabric/auth.py
@@ -7,7 +7,10 @@ def get_password(user, host, port):
     from fabric.state import env
     from fabric.network import join_host_strings
     host_string = join_host_strings(user, host, port)
-    return env.passwords.get(host_string, env.password)
+    passCall = env.passwords.get(host_string, env.password)
+    if callable(passCall):
+        return passCall(user, host, port)
+    return passCall
 
 
 def set_password(user, host, port, password):
